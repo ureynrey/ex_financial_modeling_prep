9aac43b351e2d4b8fae7dc083626d7ad4a24ebf9
Rey U <reynoldurena@Reys-MacBook-Pro.local>
2022-11-03 20:20:19 -0400

    Revamp Helpers: Binary Conversion casting

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -28,13 +28,69 @@ defmodule ExFinancialModelingPrep.Helpers do
         |> Macro.underscore()
         |> String.to_atom()
 
-      value_type = fn
-        value when is_binary(value) -> "String.t()"
-        value when is_integer(value) -> "integer()"
-        value when is_float(value) -> "float()"
+      type = cond do
+        is_float(value) -> "float()"
+        is_integer(value) -> "integer()"
+        define_string(value) == :string -> "String.t()"
+        define_string(value) == :date -> "Date.t()"
+        define_string(value) == :date_time -> "DateTime.t()"
       end
 
-      {key, value_type.(value)}
+      {key, type}
     end)
+  end
+
+  @doc """
+  Parses Binary string to appropriate type
+  """
+  @spec parse_binary(binary) :: String.t() | DateTime.t() | Date.t()
+  def parse_binary(value) do
+    define_string(value)
+    |> case do
+      :date -> to_date(value)
+      :date_time -> to_date_time(value)
+      :string -> value
+    end
+  end
+
+  @doc """
+  Informs string type :date, :date_time, :string
+  """
+  @spec define_string(binary) :: :date | :date_time | :string
+  def define_string(string) do
+    cond do
+      Regex.match?(~r/^(([0-9]{4})-([0-9]{2})-([0-9]{2}))$/, string) -> :date
+      Regex.match?(~r/^(([0-9]{4})-([0-9]{2})-([0-9]{2})) ([0-9]{2}):([0-9]{2}):([0-9]{2})$/, string) -> :date_time
+      true -> :string
+    end
+  end
+
+  @doc """
+  Converts Financial Modeling Prep date string to Date.t()
+  """
+  @spec to_date(binary) :: Date.t()
+  def to_date(string) do
+    string
+    |> String.split("-")
+    |> Enum.map(&String.to_integer/1)
+    |> then(fn [year, month, day] -> Date.new!(year, month, day) end)
+  end
+
+  @doc """
+  Converts Financial Modeling Prep date string to DateTime.t()
+  """
+  @spec to_date_time(binary) :: DateTime.t()
+  def to_date_time(string) do
+    [date_string, time] = String.split(string, " ")
+
+    [hour, min, sec] =
+      time
+      |> String.split(":")
+      |> Enum.map(&String.to_integer/1)
+
+    time = Time.new!(hour, min, sec)
+    date = to_date(date_string)
+
+    DateTime.new!(date, time)
   end
 end
