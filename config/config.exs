adfac3bdb5440df35dd87425f1c943506c3cf80f
Dave Miner <dave.miner@live.com>
2022-11-06 08:47:45 -0500

    remove exrated config and dep

--- config/config.exs
+++ config/config.exs
@@ -4,11 +4,4 @@ config :ex_financial_modeling_prep, :http_client, ExFinancialModelingPrep.API.Cl
 config :ex_financial_modeling_prep, :auth_token, ""
 config :ex_financial_modeling_prep, api_client: ExFinancialModelingPrep.API.HTTPoison
 
-config :ex_financial_modeling_prep, :ex_rated,
-  timeout: 10_000,
-  cleanup_rate: 10_000,
-  persistent: false,
-  name: :ex_rated,
-  ets_table_name: :ets_rated_test_buckets
-
 import_config "#{Mix.env()}.exs"
