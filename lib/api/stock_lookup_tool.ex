30fe1df3b4d766b119bdaa0f8315c978925cd3bd
Rey U <reynoldurena@Reys-MacBook-Pro.local>
2022-11-15 23:16:33 -0500

    Expose and add search to base module

--- lib/api/stock_lookup_tool.ex
+++ lib/api/stock_lookup_tool.ex
@@ -5,10 +5,15 @@ defmodule ExFinancialModelingPrep.Api.StockLookUpTool do
   alias ExFinancialModelingPrep.Api.Client
   alias ExFinancialModelingPrep.Struct.Search
 
-  @spec search(String.t()) :: {:ok, [Search.t()]} | {:error, HTTPoison.Error.t()}
-  def search(ticker_or_company) do
+  @doc """
+    Search via ticker and company name. Values for exchange options are:
+      ETF | MUTUAL_FUND | COMMODITY | INDEX | CRYPTO | FOREX | TSX | AMEX | NASDAQ | NYSE | EURONEXT | XETRA | NSE | LSE
+  """
+  @spec search(String.t(), [exchange: String.t()]) :: {:ok, [Search.t()]} | {:error, HTTPoison.Error.t()}
+  def search(ticker_or_company, opts) do
     query =
       %{"query" => ticker_or_company}
+      |> Map.merge(Enum.into(opts, %{}))
       |> URI.encode_query()
 
     %URI{path: "/v3/search", query: query}
