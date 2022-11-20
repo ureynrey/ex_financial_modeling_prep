7727fa8a39f09b97deff49efe80f6b628f8899b1
Rey U <reynoldurena@Reys-MacBook-Pro.local>
2022-11-20 18:47:00 -0500

    Update config version

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :ex_financial_modeling_prep, :http_client, ExFinancialModelingPrep.API.Client
-config :ex_financial_modeling_prep, :auth_token, ""
+config :ex_financial_modeling_prep, :auth_token, "a4f3822ce7adfe7d99ed313880df62fb"
 config :ex_financial_modeling_prep, api_client: ExFinancialModelingPrep.API.HTTPoison
 
 import_config "#{Mix.env()}.exs"
