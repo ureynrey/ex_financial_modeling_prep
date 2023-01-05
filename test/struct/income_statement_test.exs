e41fef9f4c4e663ec2c70f9f75eb6f74e0f49fc1
Rey U <reynoldurena@Reys-MacBook-Pro.local>
2023-01-05 00:01:28 -0500

    Mix format

--- test/struct/income_statement_test.exs
+++ test/struct/income_statement_test.exs
@@ -1,13 +1,11 @@
 defmodule ExFinancialModelingPrep.Struct.IncomeStatementTest do
-	use ExUnit.Case
-	doctest ExFinancialModelingPrep.Struct.IncomeStatement
-	alias ExFinancialModelingPrep.Struct.IncomeStatement
+  use ExUnit.Case
+  doctest ExFinancialModelingPrep.Struct.IncomeStatement
+  alias ExFinancialModelingPrep.Struct.IncomeStatement
 
   import ExFinancialModelingPrep.Factory
 
   test "factory" do
-    assert %IncomeStatement{} =
-      build(:income_statement)
-
+    assert %IncomeStatement{} = build(:income_statement)
   end
 end
